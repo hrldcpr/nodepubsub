cdd972aabaf0d3014b5626bb7b35392a9e3dff2f
Harold Cooper <hrldcpr@gmail.com>
2012-05-31 21:35:54 -0400

    remove closed getters

--- pubsub.js
+++ pubsub.js
@@ -1,10 +1,21 @@
 var http = require('http');
 
+Array.prototype.remove = function(e) {
+    for (var i = 0; i < this.length; i++) {
+	if (e == this[i]) { return this.splice(i, 1); }
+    }
+};
+
 var getters = [];
 
 http.createServer(function(request, response) {
-    if (request.method == 'GET')
+    if (request.method == 'GET') {
 	getters.push(response);
+
+	response.on('close', function() {
+	    getters.remove(response);
+	});
+    }
 
     else if (request.method == 'POST') {
 	request.on('data', function(data) {
