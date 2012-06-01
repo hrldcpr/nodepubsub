dad84abbc15c1d6a66021e252b7ddd90d25fcb13
Harold Cooper <hrldcpr@gmail.com>
2012-05-31 22:22:45 -0400

    on second thought, closing connections after each message is simpler

--- pubsub.js
+++ pubsub.js
@@ -1,31 +1,20 @@
 var http = require('http');
 
-Array.prototype.remove = function(e) {
-    for (var i = 0; i < this.length; i++) {
-	if (e == this[i]) { return this.splice(i, 1); }
-    }
-};
-
 var getters = [];
 
 http.createServer(function(request, response) {
-    if (request.method == 'GET') {
+    if (request.method == 'GET')
 	getters.push(response);
 
-	response.on('close', function() {
-	    getters.remove(response);
-	});
-    }
-
     else if (request.method == 'POST') {
-	request.on('data', function(data) {
-	    getters.forEach(function(getter) {
-		getter.write(data);
-	    });
+	console.log(getters.length + ' getters');
+	getters.forEach(function(getter) {
+	    request.pipe(getter);
 	});
 
 	request.on('end', function() {
 	    response.end();
+	    getters.splice(0, getters.length); // clear
 	});
     }
 }).listen(8080);
