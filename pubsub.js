d1a271a3cf3f813de1ea35cfc6a9b9227dcf2f79
Harold Cooper <hrldcpr@gmail.com>
2012-06-02 13:33:59 -0400

    separation by path, and hopefully leakless response pool

--- pubsub.js
+++ pubsub.js
@@ -1,27 +1,58 @@
 var fs = require('fs');
 var https = require('https');
+var url = require('url');
 
-var getters = [];
+Array.prototype.remove = function(x) {
+    for (var i in this) {
+	if (this[i] == x) return this.splice(i, 1);
+    }
+};
+
+// map from path -> [getters]
+var pathGetters = {};
+
+function printStats() {
+    for (var path in pathGetters)
+	console.log(path + ': ' + pathGetters[path].length + ' getters');
+}
 
 https.createServer({
     ca: fs.readFileSync('sub.class1.server.ca.pem'),
     key: fs.readFileSync('ssl.key'),
     cert: fs.readFileSync('ssl.crt')
 }, function(request, response) {
-    if (request.method == 'GET')
-	getters.push(response);
+    var path = url.parse(request.url).pathname;
+
+    if (request.method == 'GET') {
+	if (!pathGetters[path])
+	    pathGetters[path] = [];
+	pathGetters[path].push(response);
+	console.log('GET ' + path);
+	printStats();
+
+	// listening for 'end' doesn't work, so we have to hack:
+	response._end = response.end;
+	response.end = function() {
+	    pathGetters[path].remove(response);
+	    if (pathGetters[path].length == 0)
+		delete pathGetters[path];
+	    console.log('/GET ' + path);
+	    printStats();
+	    return response._end();
+	}
+
+	// if connection closes prematurely:
+	response.on('close', response.end);
+    }
 
     else if (request.method == 'POST') {
-	console.log('SEND to ' + getters.length + ' getters');
+	console.log('POST ' + path);
 	request.pipe(process.stdout);
-	getters.forEach(function(getter) {
-	    request.pipe(getter);
-	});
-
-	request.on('end', function() {
-	    response.end();
-	    console.log('\n/SEND');
-	    getters = [];
-	});
+	if (pathGetters[path]) {
+	    pathGetters[path].forEach(function(getter) {
+		request.pipe(getter);
+	    });
+	}
+	response.end();
     }
 }).listen(443);
