ab307147970727dca96f703c5dab9271d9cf4e3f
Harold Cooper <hrldcpr@gmail.com>
2012-06-02 12:45:58 -0400

    ssl

--- pubsub.js
+++ pubsub.js
@@ -1,20 +1,27 @@
-var http = require('http');
+var fs = require('fs');
+var https = require('https');
 
 var getters = [];
 
-http.createServer(function(request, response) {
+https.createServer({
+    ca: fs.readFileSync('sub.class1.server.ca.pem'),
+    key: fs.readFileSync('ssl.key'),
+    cert: fs.readFileSync('ssl.crt')
+}, function(request, response) {
     if (request.method == 'GET')
 	getters.push(response);
 
     else if (request.method == 'POST') {
-	console.log(getters.length + ' getters');
+	console.log('SEND to ' + getters.length + ' getters');
+	request.pipe(process.stdout);
 	getters.forEach(function(getter) {
 	    request.pipe(getter);
 	});
 
 	request.on('end', function() {
 	    response.end();
+	    console.log('\n/SEND');
 	    getters.splice(0, getters.length); // clear
 	});
     }
-}).listen(8080);
+}).listen(443);
